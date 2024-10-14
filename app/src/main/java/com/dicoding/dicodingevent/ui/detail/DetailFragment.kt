2d42c60e912c5f477d4e064a065151a56c45067b
muammarRM <muammarramadhanimaulizidan@gmail.com>
2024-10-14 21:20:14 +0700

    fix (detailevent): fix back button

--- app/src/main/java/com/dicoding/dicodingevent/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/dicoding/dicodingevent/ui/detail/DetailFragment.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -12,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.dicoding.dicodingevent.data.response.Event
@@ -37,7 +39,7 @@ class DetailFragment : Fragment() {
 
         hideBottomNavigationView()
         observeViewModel()
-
+        setHasOptionsMenu(true)
         viewModel.getEventDetail(args.eventId)
     }
 
@@ -82,6 +84,16 @@ class DetailFragment : Fragment() {
             buttonLink.setOnClickListener {
                 startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(event.link)))
             }
+        }
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                findNavController().navigateUp()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
